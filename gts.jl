5352451d6e0333a54c50090ec30412644ba0ad07
Girish Nivarti <nvgirishs@gmail.com>
2020-05-11 19:29:15 +0100

    Pass linear coeffs to linear and so on

--- gts.jl
+++ gts.jl
@@ -22,50 +22,42 @@ function lv!(du,u,p,t)
 end
 
 function L(p)
-    # calculate linear coefficient vector
-    α, β, δ, γ, τ = p
 
-    coeffs = [α 0.0;
-              0.0 -δ]
+    α, δ = p
+    return [α 0.0;
+            0.0 -δ]
 
-    return coeffs
 end
 
 function N(p)
-    # calculate linear coefficient vector
-    α, β, δ, γ, τ = p
 
-    coeffs = [0.0 -β;
-              γ 0.0]
+    β, γ = p
+    return [0.0 -β;
+             γ 0.0]
 
-    return coeffs
 end
 
 function F(p)
-    # calculate a linear forcing term
-    α, β, δ, γ, τ = p
 
-    # start with 0 forcing
-    coeffs = (0.0/τ)*[1.0 0.0;
-                0.0 1.0]
+    τ = p
+    return (0.0/τ) .* [1.0 0.0;
+                       0.0 1.0]
 
-    return coeffs
 end
 
 function glv!(du,u,p,t)
-    # Generalisation of Lotka-Volterra -> towards GQL
-    # x, y = u
-    α, β, δ, γ, τ = p
 
+    # Generalise LV towards GQL
+    α, β, δ, γ, τ = p
     # a = 0.0
-    b = L(p) * u
-    c = N(p) .* (u*u')
-    d = F(p) * u
+    b = L([α,δ]) * u
+    c = N([β,γ]) .* (u*u')
+    # d = F([τ]) * u
 
     for i ∈ [1,2]
         du[i] = b[i] + sum(c[i,:])
-        # du[2] = dy = b[2] + c[2,1] + c[2,2]
     end
+
 end
 
 u0 = [1.0,1.0]

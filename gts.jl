51e0c1b70bae970f61f7d15a5362ebe1fa54d58e
Girish Nivarti <nvgirishs@gmail.com>
2020-05-11 14:41:02 +0100

    Cleaning up Lorenz in Julia style
    Use latex characters for parameters

--- gts.jl
+++ gts.jl
@@ -2,14 +2,20 @@ using DifferentialEquations
 using Plots
 
 function lorenz!(du,u,p,t)
- du[1] = 10.0*(u[2]-u[1])
- du[2] = u[1]*(28.0-u[3]) - u[2]
- du[3] = u[1]*u[2] - (8/3)*u[3]
+    x,y,z = u
+    σ,ρ,β = p
+
+    du[1] = dx = 10.0*(y-x)
+    du[2] = dy = x*(28.0-z) - y
+    du[3] = dz = x*y - (8/3)*z
 end
 
 u0 = [1.0;0.0;0.0]
 tspan = (0.0,100.0)
-prob = ODEProblem(lorenz!,u0,tspan)
+p = [10.0,28.0,8/3]
+
+prob = ODEProblem(lorenz!,u0,tspan,p)
 sol = solve(prob)
 
 plot(sol,vars=(1,2,3))
+plot(sol,vars=(0,2))

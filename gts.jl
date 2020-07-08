b5fa210131fdf390a0f7c3d85a440b53735f1bca
Girish Nivarti <nvgirishs@gmail.com>
2020-07-08 16:43:16 +0100

    A working copy of nl_eqs

--- gts.jl
+++ gts.jl
@@ -1,128 +1,45 @@
-using DifferentialEquations, LinearAlgebra
-using StaticArrays, SparseArrays
-using BenchmarkTools, Test
+using DifferentialEquations
 using Plots
 using Einsum
 
-# function lorenz!(du,u,p,t)
-#     # Lorenz Equations
-#     x,y,z = u
-#     σ,ρ,β = p
-#
-#     du[1] = dx = σ*(y-x)
-#     du[2] = dy = x*(ρ-z) - y
-#     du[3] = dz = x*y - β*z
-# end
+function nl_eqs!(du,u,p,t)
 
-function lv!(du,u,p,t)
-    # Lotka-Volterra Equations
-    # These have a similar structure as GQL equations
-    x, y = u
-    α, β, δ, γ = p
-    du[1] = dx = α*x - β*x*y
-    du[2] = dy = -δ*y + γ*x*y
-end
+    # resolution, Coriolis parameter, kinematic viscosity
+    nx, ny, β, ν  = p
+    nx,ny = Int64(nx), Int64(ny)
 
-function L(p)
+    spanx = 0:1:nx-1
+    spany = -ny+1:1:ny-1
 
-    α, δ = p
-    return @SMatrix [α 0.0;
-                    0.0 -δ]
+    # linear coefficients
+    ω = [kx|ky ≠ 0 ? im*β*kx^2/(kx^2 + ky^2) : 0.0 + im*0.0 for kx=spanx, ky=spany]
+    v = [ComplexF64(ν*(kx^2 + ky^2)) for kx=spanx, ky=spany]
 
-end
+    # non-linear coefficients
+    A = zeros(ComplexF64,nx,2*ny-1,nx,2*ny-1,nx,2*ny-1)
 
-function N(p)
+    triads = [(kx,ky,px,py,qx,qy) for kx=spanx, ky=spany, px=spanx, py=spany, qx=spanx, qy=spany
+                if (kx|ky ≠ 0 && px|py ≠ 0 && qx|qy ≠ 0)
+                && ((kx == px + qx && ky == py + qy) || (kx == px - qx && ky == py - qy))]
 
-    β, γ = p
-    return @SMatrix [0.0 -β;
-             γ 0.0]
+    for triad ∈ triads
 
-end
+        kx,ky,px,py,qx,qy = [Float64(triad[i]) for i=1:6]
+        kpq = CartesianIndex(triad) + CartesianIndex(1,ny,1,ny,1,ny)
+        A[kpq] = -1.0/(4.0*pi)*(px*qy - py*qx)*(px^2 + py^2 - qx^2 - qy^2)/(kx^2 + ky^2)
 
-function F(p)
-
-    τ = p
-    return @SMatrix [0.0 0.0;
-                       0.0 0.0]
-
-end
-
-#! solve the equation dq = L(q) + N(q,q) + F(q)
-function glv!(du,u,p,t)
-
-    # Generalise LV towards GQL
-    α, β, δ, γ, τ = p
-
-    # a = 0.0
-    # b = copy(u)
-    b = L([α,δ]) * u
-
-    # c = copy()
-    c = N([β,γ]) .* (u*u')
-    # d = F([τ]) * u
-
-    for i ∈ [1,2]
-        du[i] = b[i] + sum(c[i,:])
     end
 
-end
-
-function nl!(du,u,p,t)
-
-    β, ν = p
-
-    nx = 3
-    ny = 3
-
-    # Rossby + diffusion
-    b = [im*β*k^2/(k^2 + l^2) - ν*(k^2 + l^2) for k in 1:nx, l in 1:ny]
-    b[1,1] = 0.0
-
-    c = zeros(ComplexF64,(nx,ny,nx,ny,nx,ny))
-    for k in 1:nx
-        for l in 1:ny
-            for m in 1:nx
-                for n in 1:ny
-
-                    i = k + m
-                    j = l + n
-
-                    if i <= nx && j <= ny
-                        c[i,j,k,l,m,n] = - Float64(((k^2 + l^2)^2 - (m^2 + n^2)^2)/(i^2 + j^2))
-                        # @show i,j,k,l,m,n,c[i,j,k,l,m,n]
-                    end
-
-                    i = k - m
-                    j = l - n
-
-                    if i > 1 && j > 1
-                        c[i,j,k,l,m,n] = - Float64(((k^2 + l^2)^2 - (m^2 + n^2)^2)/(i^2 + j^2))
-                        # @show i,j,k,l,m,n
-                    end
-
-                end
-            end
-        end
-    end
-
-    # @show c
-    du .= b .* u
-    # @einsum du[i,j] = b[i,j]*u[i,j] + c[i,j,k,l,m,n]*u[k,l]*u[m,n]
-    @einsum du[i,j] += c[i,j,k,l,m,n]*u[k,l]*u[m,n]
+    # tendency equation (optimise later using staticarrays)
+    @einsum du[i,j] = -ω[i,j]*u[i,j] - v[i,j]*u[i,j] + A[i,j,k,l,m,n]*u[k,l]*u[m,n]
 
 end
 
-nx = 3
-ny = 3
+u0 = rand(ComplexF64,nx,2*ny-1)
+p = [2,2,1.0e-4,1.5e-5]
 
-u0 = rand(ComplexF64,(nx,ny))
-p = [1.0,0.0001]
+tspan = (0.0,1000.0)
+prob = ODEProblem(nl_eqs!,u0,tspan,p)
+sol  = solve(prob,Tsit5(),adaptive=true,stiff=true)
 
-tspan   = (0.0,1000.0)
-prob = ODEProblem(nl!,u0,tspan,p)
-sol  = solve(prob,RK4(),adaptive=true)
-
-@show size(sol)[3]
-plot(sol)
-r = [(sol[2,1,i]) for i in 1:size(sol)[3]]
-plot(r)
+plot(sol,vars=[1,4],linewidth=1,legend=false)

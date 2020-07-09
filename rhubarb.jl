29c92c581f150e89440f83d45a796689bec76c6c
Girish Nivarti <nvgirishs@gmail.com>
2020-07-09 17:53:28 +0100

    Added non-linear terms into equation

--- rhubarb.jl
+++ rhubarb.jl
@@ -1,6 +1,6 @@
 using DifferentialEquations
-using Plots
-using Einsum
+using Plots, PyPlot
+using Einsum, LinearAlgebra
 
 function nl_eqs!(du,u,p,t)
 
@@ -11,6 +11,9 @@ function nl_eqs!(du,u,p,t)
     spanx = range(0,nx-1,step=1)
     spany = range(-ny+1,ny-1,step=1)
 
+    # forcing
+    γ = []
+
     # linear coefficients
     ω = zeros(ComplexF64,nx,2*ny-1)
     ω = [kx|ky ≠ 0 ? im*β*kx^2/(kx^2 + ky^2) : 0.0 + im*0.0 for kx=spanx, ky=spany]
@@ -19,8 +22,9 @@ function nl_eqs!(du,u,p,t)
     v = [ComplexF64(ν*(kx^2 + ky^2)) for kx=spanx, ky=spany]
 
     # non-linear coefficients
-    A = zeros(ComplexF64,nx,2*ny-1,nx,2*ny-1,nx,2*ny-1)
+    A = zeros(Float64,nx,2*ny-1,nx,2*ny-1,nx,2*ny-1)
 
+    # triads obeying selection rules
     triads = [(kx,ky,px,py,qx,qy) for kx=spanx, ky=spany, px=spanx, py=spany, qx=spanx, qy=spany
                 if (kx|ky ≠ 0 && px|py ≠ 0 && qx|qy ≠ 0)
                 && ((kx == px + qx && ky == py + qy) || (kx == px - qx && ky == py - qy))]
@@ -29,27 +33,42 @@ function nl_eqs!(du,u,p,t)
 
         kx,ky,px,py,qx,qy = [Float64(mode) for mode ∈ triad]
         kpq = CartesianIndex(triad) + CartesianIndex(1,ny,1,ny,1,ny)
-        A[kpq] = -1.0/(4.0*pi)*(px*qy - py*qx)*(px^2 + py^2 - qx^2 - qy^2)/(kx^2 + ky^2)
+
+        # kx,ky = assign here rather than use indices
+        A[kpq] = -1.0/(100.0)*(px*qy - py*qx)*(px^2 + py^2 - qx^2 - qy^2)/(kx^2 + ky^2)
 
     end
 
-    # c = zeros(ComplexF64,nx,2*ny-1)
-    # @einsum c[i,j] =  A[i,j,k,l,m,n]*u[k,l]*u[m,n]
+    c = zeros(ComplexF64,nx,2*ny-1)
+    @einsum c[i,j] := A[i,j,k,l,m,n]*u[k,l]*u[m,n]
 
     @show t
     # @show size(du), size(ω), size(v), size(A)
-    # tendency equation (optimise later using sparsearrays)
-    du .= - ω .* u - v .* u
+
+    # tendency equation (to be optimised using sparsearrays+BLAS)
+    du .= - ω .* u - v .* u + c
 
 end
 
-nx,ny = 2,3
+# compute absolute vorticity
+# function q(ψ):
+    # β
+# end
 
-u0 = randn(ComplexF64,nx,2*ny-1)
-p = [nx,ny,1.0e-2,5e-4]
+# initialise problem
+nx,ny = 2,2
 
-tspan = (0.0,1000.0)
+u0      = randn(ComplexF64,nx,2*ny-1)
+p       = [nx,ny,1.0e-2,5e-3]
+tspan   = (0.0,1000.0)
+
+# choose equations and solve
 prob = ODEProblem(nl_eqs!,u0,tspan,p)
-sol  = solve(prob,BS3(),adaptive=true)
+sol  = solve(prob,RK4(),adaptive=true)
 
+# concatenate conjugate matrix
+# transforum to spatial coordinates
+
+# plot
+pyplot()
 plot(sol,linewidth=1,legend=false)

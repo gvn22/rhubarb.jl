ff008b8e20c84dc88c9bf22a8b5257fbee2b9745
Girish Nivarti <nvgirishs@gmail.com>
2020-07-13 15:28:37 +0100

    Retracing steps towards sparse formulation

--- rhubarb.jl
+++ rhubarb.jl
@@ -12,41 +12,55 @@ function nl_eqs!(du,u,p,t)
     spany = range(-ny+1,ny-1,step=1)
 
     # forcing
-    γ = []
+    # γ = []
 
     # linear coefficients
-    ω = zeros(ComplexF64,nx,2*ny-1)
-    ω = [kx|ky ≠ 0 ? im*β*kx^2/(kx^2 + ky^2) : 0.0 + im*0.0 for kx=spanx, ky=spany]
+    # ω = zeros(ComplexF64,nx,2*ny-1)
+    # ω = [kx|ky ≠ 0 ? im*β*kx^2/(kx^2 + ky^2) : 0.0 + im*0.0 for kx=spanx, ky=spany]
 
-    v = zeros(ComplexF64,nx,2*ny-1)
-    v = [ComplexF64(ν*(kx^2 + ky^2)) for kx=spanx, ky=spany]
+    # v = zeros(ComplexF64,nx,2*ny-1)
+    # v = [ComplexF64(ν*(kx^2 + ky^2)) for kx=spanx, ky=spany]
 
     # non-linear coefficients
-    A = zeros(Float64,nx,2*ny-1,nx,2*ny-1,nx,2*ny-1)
+    # A = zeros(Float64,nx,2*ny-1,nx,2*ny-1,nx,2*ny-1)
 
     # triads obeying selection rules
-    triads = [(kx,ky,px,py,qx,qy) for kx=spanx, ky=spany, px=spanx, py=spany, qx=spanx, qy=spany
+    # triads = [(kx,ky,px,py,qx,qy) for kx=spanx, ky=spany, px=spanx, py=spany, qx=spanx, qy=spany
+    #             if (kx|ky ≠ 0 && px|py ≠ 0 && qx|qy ≠ 0)
+    #             && ((kx == px + qx && ky == py + qy) || (kx == px - qx && ky == py - qy))]
+    println("Constructing set of triads...")
+
+    tri_adds =  [(k = (kx,ky),p = (px,py), q = (qx,qy))
+                for qx=spanx, qy=spany, px=spanx, py=spany, kx=spanx, ky=spany
                 if (kx|ky ≠ 0 && px|py ≠ 0 && qx|qy ≠ 0)
-                && ((kx == px + qx && ky == py + qy) || (kx == px - qx && ky == py - qy))]
+                && (kx == px + qx && ky == py + qy)]
 
-    for triad ∈ triads
+    tri_difs =  [(k = (kx,ky),p = (px,py), q = (qx,qy))
+                for qx=spanx, qy=spany, px=spanx, py=spany, kx=spanx, ky=spany
+                if (kx|ky ≠ 0 && px|py ≠ 0 && qx|qy ≠ 0)
+                && (kx == px - qx && ky == py - qy)]
 
-        kx,ky,px,py,qx,qy = [Float64(mode) for mode ∈ triad]
-        kpq = CartesianIndex(triad) + CartesianIndex(1,ny,1,ny,1,ny)
+    triads = vcat(tri_adds,tri_difs)
+    # @show length(triads),3*5*3*5*3*5*2
 
-        # kx,ky = assign here rather than use indices
-        A[kpq] = -1.0/(100.0)*(px*qy - py*qx)*(px^2 + py^2 - qx^2 - qy^2)/(kx^2 + ky^2)
-
-    end
-
-    c = zeros(ComplexF64,nx,2*ny-1)
-    @einsum c[i,j] := A[i,j,k,l,m,n]*u[k,l]*u[m,n]
-
-    @show t
-    # @show size(du), size(ω), size(v), size(A)
-
-    # tendency equation (to be optimised using sparsearrays+BLAS)
-    du .= - ω .* u - v .* u + c
+    # for triad ∈ triads
+    #
+    #     kx,ky,px,py,qx,qy = [Float64(mode) for mode ∈ triad]
+    #     kpq = CartesianIndex(triad) + CartesianIndex(1,ny,1,ny,1,ny)
+    #
+    #     # kx,ky = assign here rather than use indices
+    #     A[kpq] = -1.0/(100.0)*(px*qy - py*qx)*(px^2 + py^2 - qx^2 - qy^2)/(kx^2 + ky^2)
+    #
+    # end
+    #
+    # c = zeros(ComplexF64,nx,2*ny-1)
+    # @einsum c[i,j] := A[i,j,k,l,m,n]*u[k,l]*u[m,n]
+    #
+    # @show t
+    # # @show size(du), size(ω), size(v), size(A)
+    #
+    # # tendency equation (to be optimised using sparsearrays+BLAS)
+    # du .= - ω .* u - v .* u + c
 
 end
 
@@ -56,19 +70,19 @@ end
 # end
 
 # initialise problem
-nx,ny = 2,2
+nx,ny = 3,3
 
 u0      = randn(ComplexF64,nx,2*ny-1)
 p       = [nx,ny,1.0e-2,5e-3]
-tspan   = (0.0,1000.0)
+tspan   = (0.0,1.0)
 
 # choose equations and solve
 prob = ODEProblem(nl_eqs!,u0,tspan,p)
-sol  = solve(prob,RK4(),adaptive=true)
+sol  = solve(prob,RK4(),adaptive=false,dt=0.01,maxiters=10)
 
 # concatenate conjugate matrix
 # transforum to spatial coordinates
 
 # plot
-pyplot()
-plot(sol,linewidth=1,legend=false)
+# pyplot()
+# plot(sol,linewidth=1,legend=false)

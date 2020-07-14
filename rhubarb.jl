3355a0f1767b52e5ecca4b6136beedd300905507
Girish Nivarti <nvgirishs@gmail.com>
2020-07-14 17:09:26 +0100

    Testing (failed) for conservation of E and Z

--- rhubarb.jl
+++ rhubarb.jl
@@ -2,165 +2,150 @@ using DifferentialEquations
 using Plots, PyPlot
 using Einsum, LinearAlgebra
 
-function nl_eqs!(du,u,p,t)
-
-    # parameters
-    nx, ny, β, ν  = p
-    nx,ny = Int64(nx), Int64(ny)
-
-    spanx = range(0,nx-1,step=1)
-    spany = range(-ny+1,ny-1,step=1)
-
-    # forcing
-    # γ = []
+function l_coeffs(β,ν,spanx,spany)
 
     # linear coefficients
-    # ω = zeros(ComplexF64,nx,2*ny-1)
-    # ω = [kx|ky ≠ 0 ? im*β*kx^2/(kx^2 + ky^2) : 0.0 + im*0.0 for kx=spanx, ky=spany]
+    # ω = fill!(similar(u),0)
+    ω = [kx|ky ≠ 0 ? im*β*kx^2/(kx^2 + ky^2) : 0.0 for ky=spany for kx=spanx]
 
-    # v = zeros(ComplexF64,nx,2*ny-1)
-    # v = [ComplexF64(ν*(kx^2 + ky^2)) for kx=spanx, ky=spany]
+    # v = fill!(similar(u),0)
+    # v = [ComplexF64(ν*(kx^2 + ky^2)) for ky=spany for kx=spanx]
 
-    # non-linear coefficients
-    # A = zeros(Float64,nx,2*ny-1,nx,2*ny-1,nx,2*ny-1)
+    return ω
 
-    # triads obeying selection rules
-    # triads = [(kx,ky,px,py,qx,qy) for kx=spanx, ky=spany, px=spanx, py=spany, qx=spanx, qy=spany
-    #             if (kx|ky ≠ 0 && px|py ≠ 0 && qx|qy ≠ 0)
-    #             && ((kx == px + qx && ky == py + qy) || (kx == px - qx && ky == py - qy))]
-    println("Constructing set of triads...")
+end
 
-    tri_adds =  [(k = (kx,ky),p = (px,py), q = (qx,qy))
-                for qx=spanx, qy=spany, px=spanx, py=spany, kx=spanx, ky=spany
-                if (kx|ky ≠ 0 && px|py ≠ 0 && qx|qy ≠ 0)
-                && (kx == px + qx && ky == py + qy)]
+function nl_coeffs(triads)
 
-    tri_difs =  [(k = (kx,ky),p = (px,py), q = (qx,qy))
-                for qx=spanx, qy=spany, px=spanx, py=spany, kx=spanx, ky=spany
-                if (kx|ky ≠ 0 && px|py ≠ 0 && qx|qy ≠ 0)
-                && (kx == px - qx && ky == py - qy)]
+    # println("Computing coefficients...")
+    function pos(p)
+        return p[1]*(2*ny-1) + (p[2] + ny)
+    end
 
-    # triads = vcat(tri_adds,tri_difs)
+    Ks  = Int[]
+    Cs  = SparseMatrixCSC{ComplexF64}[]
 
-    Kx  = Int[]
-    Ky  = Int[]
-    Cp  = SparseMatrixCSC{ComplexF64}[]
+    Ps  = Int[]
+    Qs  = Int[]
+    As  = ComplexF64[]
+    for (i,triad) ∈ enumerate(triads)
 
-    Pv  = Int[]
-    Qv  = Int[]
-    A   = ComplexF64[]
-    for (i,triad) ∈ enumerate(tri_adds)
+        kx,ky   = ComplexF64(triad.k[1]),ComplexF64(triad.k[2])
+        px,py   = ComplexF64(triad.p[1]),ComplexF64(triad.p[2])
+        qx,qy   = ComplexF64(triad.q[1]),ComplexF64(triad.q[2])
+        # akpq    = -0.5*((kx*py - ky*px)/(px^2 + py^2) + (kx*qy - ky*qx)/(qx^2 + qy^2))
+        akpq    = 0.5*((px*qy - qx*py)*(qx^2 + qy^2 - px^2 - py^2)/(kx^2 + ky^2))
 
-        kx,ky = triad.k[1] + 1,triad.k[2] + ny
-        px,py = triad.p[1] + 1,triad.p[2] + ny
-        qx,qy = triad.q[1] + 1,triad.q[2] + ny
+        P,Q = triad.p, triad.q
 
-        push!(Pv,px*(2*ny-1) + py)
-        push!(Qv,qx*(2*ny-1) + qy)
+        push!(Ps,pos(P))
+        push!(Qs,pos(Q))
+        push!(As, akpq)
 
-        akpq = -1.0/(4.0*pi)*(px*qy - py*qx)*(px^2 + py^2 - qx^2 - qy^2)/(kx^2 + ky^2)
-
-        push!(A, akpq)
-
-        next = i < length(tri_adds) ? tri_adds[i + 1] : nothing
+        next = i < length(triads) ? triads[i + 1] : nothing
         if(next == nothing || next.k ≠ triad.k)
 
-            Cp = sparse(Pv,Qv,A,(nx+1)*(2*ny),(nx+1)*(2*ny))
+            A = sparse(Ps,Qs,As,nx*(2*ny-1),nx*(2*ny-1))
 
-            Pv = Int[]
-            Qv = Int[]
+            Ps = Int[]
+            Qs = Int[]
+            As = ComplexF64[]
 
-            A = ComplexF64[]
-
-            push!(Kx,kx)
-            push!(Ky,ky)
+            K = triad.k
+            push!(Ks,pos(K))
+            push!(Cs,A)
 
         end
 
     end
 
-    kv = Int[]
-    nlv = ComplexF64[]
-    c = similar(u)
-
-    for (i,k) in enumerate(zip(Kx,Ky))
-
-        push!(kv, k[1]*(2*ny - 1) + k[2])
-
-        mul!(c,Cp[i],u)
-        push!(nlv,dot(c,u))
-
-    end
-
-    nl = sparsevec(kv,nlv,(nx+1)*(2*ny))
-    du .= nl
-
-    # Bp = [dot(mul!(B,c,u),u) for c in Cp]
-
-    # println ("")
-    # @show unique(x->x.k,tri_adds)
-    # for triad ∈ tri_adds
-    #
-    #     for
-    #         kx,ky = triad.k
-    #         px,py = triad.p
-    #         qx,qy = triad.q
-    #
-    #         push!(Pv,px*(2*ny-1) + py)
-    #         push!(Qv,qx*(2*ny-1) + qy)
-    #
-    #         akpq = -1.0/(4.0*pi)*(px*qy - py*qx)*(px^2 + py^2 - qx^2 - qy^2)/(kx^2 + ky^2)
-    #
-    #         push!(Cpv, akpq)
-    #     end
-    #     push!(Cp,sparse(A)
-    #
-    # end
-    #
-    # Kx = []
-    # Ky = []
-    # Cp = SparseMatrixCSC{ComplexF64}[]
-    #
-    # for triad ∈ tri_adds
-    #
-    #     kx,ky = triad.k
-    #
-    #     Kxv[i] = kx
-    #     Kyv[i] = ky
-    #
-    #
-    # end
-
-    # c = zeros(ComplexF64,nx,2*ny-1)
-    # @einsum c[i,j] := A[i,j,k,l,m,n]*u[k,l]*u[m,n]
-    #
-    # @show t
-    # # @show size(du), size(ω), size(v), size(A)
-    #
-    # # tendency equation (to be optimised using sparsearrays+BLAS)
-    # du .= - ω .* u - v .* u + c
+    return Ks,Cs
 
 end
 
-# compute absolute vorticity
-# function q(ψ):
-    # β
-# end
+function nl_eqs!(du,u,p,t)
 
-# initialise problem
-nx,ny = 2,2
+    nx, ny, β, ν  = p
+    nx,ny = Int64(nx), Int64(ny)
+    spanx   = range(0,nx-1,step=1)
+    spany   = range(-ny+1,ny-1,step=1)
 
-u0      = randn(ComplexF64,(nx+1)*(2*ny))
-p       = [nx,ny,1.0e-2,5e-3]
+    E = sum(kx|ky ≠ 0 ? u[kx*(2*ny - 1) + ky+ny]^2/(kx^2 + ky^2) : 0.0 for ky=spany for kx=spanx)
+    Z = sum(kx|ky ≠ 0 ? u[kx*(2*ny - 1) + ky+ny]^2 : 0.0 for ky=spany for kx=spanx)
+
+    @show E,Z
+
+    # modes: k = p + q
+    Bp      = ComplexF64[]
+    temp    = similar(u)
+
+    for (mode,matrix) in zip(Kp,Cp)
+
+        mul!(temp,matrix,u)
+        push!(Bp,dot(temp,u))
+
+    end
+
+    Σp = sparsevec(Kp,Bp,(nx)*(2*ny-1))
+    # @show Kp,Σp
+
+    # modes: k = p - q
+    Bm      = ComplexF64[]
+    temp    = similar(u)
+
+    for (mode,matrix) in zip(Km,Cm)
+
+        mul!(temp,matrix,conj!(u))
+        push!(Bm,dot(temp,u))
+
+    end
+
+    Σm = sparsevec(Km,Bm,(nx)*(2*ny-1))
+    # @show Km,Σm
+
+    du .= Σp + Σm
+
+    # q = u + β
+    # @show Γ2
+
+end
+
+nx,ny   = 3,3
+β,ν     = 1.0e-3,5e-3
+p       = [nx,ny,β,ν]
+
+X,Y     = nx,2*ny - 1
+u0      = randn(ComplexF64,X*Y)
+
 tspan   = (0.0,10.0)
 
-# choose equations and solve
-prob = ODEProblem(nl_eqs!,u0,tspan,p)
-sol  = solve(prob,RK4(),adaptive=true)
+spanx   = range(0,nx-1,step=1)
+spany   = range(-ny+1,ny-1,step=1)
 
-# concatenate conjugate matrix
-# transforum to spatial coordinates
+Δp      =  [(k=(kx,ky),p=(px,py),q=(qx,qy))
+            for qx=spanx, qy=spany, px=spanx, py=spany, kx=spanx, ky=spany
+            if (kx|ky ≠ 0 && px|py ≠ 0 && qx|qy ≠ 0)
+            && (kx == px + qx && ky == py + qy)]
 
-# plot
-pyplot()
-plot(sol,linewidth=1,legend=false)
+Kp,Cp   = nl_coeffs(Δp)
+# @show Δp,Kp
+
+Δm      =  [(k = (kx,ky),p = (px,py), q = (qx,qy))
+            for qx=spanx, qy=spany, px=spanx, py=spany, kx=spanx, ky=spany
+            if (kx|ky ≠ 0 && px|py ≠ 0 && qx|qy ≠ 0)
+            && (kx == px - qx && ky == py + qy)]
+
+Km,Cm   = nl_coeffs(Δm)
+# @show Δm,Km
+
+allΔs   = vcat(Δp,Δm)
+# @show unique(x->x.k,tri_adds)
+
+@show Δp
+
+@show Δm
+prob    = ODEProblem(nl_eqs!,u0,tspan,p)
+sol     = solve(prob,Tsit5(),adaptive=true)
+
+# pyplot()
+# plot(sol,linewidth=1,legend=false)

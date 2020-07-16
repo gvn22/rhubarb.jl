cf65de46c8dd8231c19ed682b753d0c51761449c
Girish Nivarti <nvgirishs@gmail.com>
2020-07-16 11:50:55 +0100

    Debugged conjugate transpose mistake in mat mul

--- rhubarb.jl
+++ rhubarb.jl
@@ -56,59 +56,81 @@ end
 
 function nl_eqs!(du,u,p,t)
 
-    function ind(p)
-        return div(p-1,2*ny-1),mod(p-1,2*ny-1)-ny+1
+    function ind(x)
+        return div(x-1,2*ny-1),mod(x-1,2*ny-1)-ny+1
     end
 
     nx, ny, Bωv, Cp, Cm  = p
     N = nx*(2*ny-1)
 
     Σp,Σm   = fill!(similar(u),0),fill!(similar(u),0)
+    # @show u
 
-    # modes: k = p + q
+    # println("Modes: k = p + q")
     for (k,c) ∈ Cp
 
-        @show k,ind(k)
-        temp = fill!(similar(u),0)
+        # @show k,ind(k)
+        # @show c
+        # temp = fill!(similar(u),0)
+
+        # @show (c .* u) .* transpose(u)
+        # @show issparse((c .* u) .* transpose(u))
+        # @show issparse(c)
+        # temp = sum(nonzeros((c .* u) .* u'))
+        # @show temp
+        Σp[k] = sum(nonzeros((c .* u) .* transpose(u)))
+
+        # @show Σp[k]
 
         # mul!(temp,c',u)
         # Σp[k] = dot(temp,u)
         # Σp[k] = dot(u,BLAS.gemv('T',c,u))
 
-        mul!(temp,transpose(c),u)
-        Σp[k] = BLAS.dotu(N,u,1,temp,1)
+        # mul!(temp,transpose(c),u)
+        # Σp[k] = BLAS.dotu(N,u,1,temp,1)
 
     end
 
-    # modes: k = p - q
+    # println("Modes: k = p - q")
     for (k,c) ∈ Cm
 
-        temp = fill!(similar(u),0)
-        @show k,ind(k)
+        # temp = fill!(similar(u),0)
+        # @show k,ind(k)
+        # @show c
+        # @show (c .* u) .* transpose(conj(u))
+        # @show issparse((c .* u) .* transpose(conj(u)))
+        # @show sum(nonzeros((c .* u) .* transpose(conj(u)))) # @show temp
+        Σm[k] = sum(nonzeros((c .* u) .* transpose(conj(u))))
 
-        mul!(temp,transpose(c),u)
-        Σm[k] = BLAS.dotc(N,u,1,temp,1)
+        # @show Σm[k]
+        # mul!(temp,transpose(c),u)
+        # Σm[k] = BLAS.dotc(N,u,1,temp,1)
 
     end
 
-    Σ       = BLAS.axpy!(1.0,Σp,Σm)
-    temp    = BLAS.axpy!(1.0,Bωv.*u,Σ)
+    for k ∈ 1:nx*(2*ny-1)
+        du[k] = Σp[k] + Σm[k]
+    end
 
+    # @show du
+    # Σ       = BLAS.axpy!(1.0,Σp,Σm)
+    # temp    = BLAS.axpy!(1.0,Bωv.*u,Σ)
     # @show temp
     # du .= ωv.*u .+ Σ
     # du .= fill!(similar(u),0)
-    @time du      .= temp
+    # @time du      .= temp
 
     spanx   = range(0,nx-1,step=1)
     spany   = range(-ny+1,ny-1,step=1)
 
-    E = sum(ind(k)[1]|ind(k)[2] ≠ 0 ? abs(u[k])^2/(ind(k)[1]^2 + ind(k)[2]^2) : 0.0 for k ∈ 1:nx*(2*ny-1))
+    E = sum(ind(k)[1]|ind(k)[2] ≠ 0 ? u[k]*conj(u[k])/(ind(k)[1]^2 + ind(k)[2]^2) : 0.0 for k ∈ 1:nx*(2*ny-1))
     Z = sum(abs(u[k])^2 for k ∈ 1:nx*(2*ny-1))
 
     @show t, E,Z
 
 end
 
+# BLAS.set_num_threads(1)
 # resolution
 nx,ny   = 2,2
 spanx   = range(0,nx-1,step=1)
@@ -143,18 +165,22 @@ Cm      = nl_coeffs(Δm,-1)
 
 # setup and solve equations
 u0      = randn(ComplexF64,nx*(2*ny-1))
+# u0      = ones(ComplexF64,nx*(2*ny-1))
+
 tspan   = (0.0,20.0)
 p       = [nx,ny,Bωv,Cp,Cm]
 
 prob    = ODEProblem(nl_eqs!,u0,tspan,p)
-sol     = @time solve(prob,Tsit5(),adaptive=true)
+sol     = solve(prob,Tsit5())
+# integrator = init(prob,RK4())
+# step!(integrator)
 
 Plots.plot(sol,vars=(0,1),linewidth=4,label="(0,-1)",legend=true)
-Plots.plot!(sol,vars=(0,2),linewidth=4,label="(0,0)",legend=true)
-Plots.plot!(sol,vars=(0,3),linewidth=4,label="(0,1)",legend=true)
-Plots.plot!(sol,vars=(0,4),linewidth=4,label="(1,-1)",legend=true)
-Plots.plot!(sol,vars=(0,5),linewidth=4,label="(1,0)",legend=true)
-Plots.plot!(sol,vars=(0,6),linewidth=4,label="(1,1)",legend=true)
+Plots.plot!(sol,vars=(0,2),linewidth=4,label="(0,0)")
+Plots.plot!(sol,vars=(0,3),linewidth=4,label="(0,1)")
+Plots.plot!(sol,vars=(0,4),linewidth=4,label="(1,-1)")
+Plots.plot!(sol,vars=(0,5),linewidth=4,label="(1,0)")
+Plots.plot!(sol,vars=(0,6),linewidth=4,label="(1,1)")
 
 # f(x,y)  = (x,abs(y)^2)
 # Plots.plot(sol,vars=(f,1,2)linewidth=4,legend=true)

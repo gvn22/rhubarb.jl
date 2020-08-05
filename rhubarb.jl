42661d73634df65bbcffcc675d3ed061e8a84a2f
Girish Nivarti <nvgirishs@gmail.com>
2020-08-05 19:26:52 +0100

    Debug in X dir, and moved to 2d array with Mx2N-1 entries

--- rhubarb.jl
+++ rhubarb.jl
@@ -1,4 +1,4 @@
-using DifferentialEquations
+using DifferentialEquations,RecursiveArrayTools
 using Plots; plotly()
 
 function nl_coeffs(X,Y,M,N)
@@ -25,11 +25,14 @@ function nl_coeffs(X,Y,M,N)
 
                         push!(Δp,[m,n,m1,n1,m2,n2])
 
-                        px,py   = (2.0*pi/X)*m1,(2.0*pi/Y)*n1
-                        qx,qy   = (2.0*pi/X)*m2,(2.0*pi/Y)*n2
+                        px,py   = (2.0*pi/X)*Float64(m1),(2.0*pi/Y)*Float64(n1)
+                        qx,qy   = (2.0*pi/X)*Float64(m2),(2.0*pi/Y)*Float64(n2)
 
-                        c       = -(px*qy - qx*py)*(1.0/(px^2 + py^2) - 1.0/(qx^2 + qy^2))
-
+                        if m1 ≠ m2
+                            c       = -(px*qy - qx*py)*(1.0/(px^2 + py^2) - 1.0/(qx^2 + qy^2))
+                        else
+                            c       = -(px*qy - qx*py)/(px^2 + py^2)
+                        end
                         push!(Cp,c)
 
                         println("[",m,",",n,"] = [",m1,",",n1,"] + [",m2,",",n2,"] -> ",c)
@@ -89,18 +92,16 @@ function nl_eqs!(du,u,p,t)
 
     nx, ny, Cp, Cm  = p
 
-    function pos(a,b)
-        return a*(2*ny-1) + (b + ny)
-    end
-
-    u[1] = u[3]
-    dq = fill!(similar(u),0)
+    dζ = fill!(similar(du),0)
 
     for (Δ,C) ∈ Cp
 
         # @show Δ, C
-        k,p,q   = pos(Δ[1],Δ[2]),pos(Δ[3],Δ[4]),pos(Δ[5],Δ[6])
-        dq[k]   = dq[k] + C*u[p]*u[q]
+        m,n     = Δ[1] + 1, Δ[2] + ny
+        m1,n1   = Δ[3] + 1, Δ[4] + ny
+        m2,n2   = Δ[5] + 1, Δ[6] + ny
+
+        dζ[m,n] += C*u[m1,n1]*u[m2,n2]
         # println("m = ", Δ[1],", n = ", Δ[2],", p = ", k, ", dζ += ", C*u[p]*u[q], "\n")
 
     end
@@ -109,33 +110,34 @@ function nl_eqs!(du,u,p,t)
 
         # @show Δ, C
 
-        k,p,q   = pos(Δ[1],Δ[2]),pos(Δ[3],Δ[4]),pos(Δ[5],Δ[6])
-        dq[k]   = dq[k] + C*u[p]*conj(u[q])
+        m,n     = Δ[1] + 1, Δ[2] + ny
+        m1,n1   = Δ[3] + 1, Δ[4] + ny
+        m2,n2   = Δ[5] + 1, Δ[6] + ny
+
+        dζ[m,n] += C*u[m1,n1]*conj(u[m2,n2])
 
         # println("m = ", Δ[1],", n = ", Δ[2],", p = ", k, ", dζ += ", C*u[p]*conj(u[q]), "\n")
 
     end
 
-    du .= dq
-    du[1] = dq[3]
+    du .= dζ
 
 end
 
 Lx,Ly   = 2.0*pi,2.0*pi
-nx,ny   = 2,2
+nx,ny   = 3,2
+# u0    = [1.0,0.0,1.0,2.0,3.0,4.0]
+u0      = randn(ComplexF64,nx,(2*ny-1))
+tspan   = (0.0,100.0)
 
 C1,C2   = nl_coeffs(Lx,Ly,nx-1,ny-1)
-
-u0      = randn(ComplexF64,nx*(2*ny-1))
-
-# u0      = [1.0,0.0,1.0,2.0,3.0,4.0]
-tspan   = (0.0,100.0)
 p       = [nx,ny,C1,C2]
 
 prob    = ODEProblem(nl_eqs!,u0,tspan,p)
 sol     = solve(prob,RK4(),adaptive=true,progress=true,reltol=1e-6,abstol=1e-6)
-integrator = init(prob,RK4())
-step!(integrator)
+
+# integrator = init(prob,RK4())
+# step!(integrator)
 
 Plots.plot(sol,vars=(0,1),linewidth=2,label="(0,-1)",legend=true)
 Plots.plot!(sol,vars=(0,2),linewidth=2,label="(0,0)")
@@ -144,26 +146,24 @@ Plots.plot!(sol,vars=(0,4),linewidth=2,label="(1,-1)")
 Plots.plot!(sol,vars=(0,5),linewidth=2,label="(1,0)")
 Plots.plot!(sol,vars=(0,6),linewidth=2,label="(1,1)")
 
-cx,cy = 2.0*pi/Lx,2.0*pi/Ly
-
-function ind(x)
-    return div(x-1,2*ny-1),mod(x-1,2*ny-1)-ny+1
-end
 
 E = zeros(Float64,length(sol.u))
 Z = zeros(Float64,length(sol.u))
 
 for (i,u) ∈ enumerate(sol.u)
 
-    for k ∈ ny+1:1:nx*(2*ny-1)
+    for j ∈ 0:1:nx-1
+        kmin = j == 0 ? 1 : -ny + 1
+        for k ∈ kmin:1:ny-1
 
-        m,n   = ind(k)
-        if !(m|n == 0)
+            m,n   = j + 1, k + ny
 
-            E[i] += abs(u[k])^2/(m^2 + n^2)
-            Z[i] += abs(u[k])^2
+            cx,cy = (2.0*pi/Lx)*j,(2.0*pi/Ly)*k
+
+            E[i] += abs(u[m,n])^2/(cx^2 + cy^2)
+            Z[i] += abs(u[m,n])^2
+
         end
-
     end
 end
 
